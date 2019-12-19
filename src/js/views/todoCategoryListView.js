67d9c3bbc9f146067bc2c9327b761fa714f50f4d
Jorre Spijker <jorre.spijker@foursites.nl>
2019-12-19 11:47:09 +0100

    Removed old stuff

--- src/js/views/todoCategoryListView.js
+++
@@ -1,7 +0,0 @@
-import { elements } from './base';
-import { CONFIG } from '../config';
-
-export const renderListItem = (item) => {
-    const markup = `<option value="${item.title}" ${item.title === CONFIG.settings.categories.default ? 'selected' : ''}>${item.title}</option>`;
-    elements.todo.categoryForm.insertAdjacentHTML("afterbegin", markup);
-}
